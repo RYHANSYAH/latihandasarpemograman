fd931a284151bb35319a9b786eac82f7390c0f2d
RYHANSYAH <114573108+RYHANSYAH@users.noreply.github.com>
2022-09-29 11:11:24 +0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,16 @@
+# aplikasi biodata creat by rayhansyah
+
+# memasukan nama
+print("Yuk!lengkapi data diri..")
+
+# input data diri
+nama = input("nama                      :")
+kelas = input("kelas                    :")
+prodi = input("prodi                    :")
+
+# print / output nama
+print("nama anda adalah:",nama)
+print("kelas anda adalah:",kelas)
+print("prodi anda adalah:",prodi)
+
+
