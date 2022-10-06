9746f39ee4c2a1f9c7890a50125306c95fb9583f
RYHANSYAH <114573108+RYHANSYAH@users.noreply.github.com>
2022-10-06 09:20:28 +0700

    Add files via upload

---
+++ Tugas_Sesi3.py
@@ -0,0 +1,11 @@
+# Tugas sesi 3 by RAYHANSYAH
+
+nama = "RAYHANSYAH"
+kelas = "TI22A"
+prodi = "Teknik Informatika"
+quote = "jika sudah terwujud jangan lupa bersujud"
+
+print("nama     :",nama)
+print("kelas    :",kelas)
+print("prodi    :",prodi)
+print("quote    :",quote)
